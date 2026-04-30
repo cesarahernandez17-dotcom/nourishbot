256f914eac1649bfaca5838c52811877d2198a07
César Hernández <cesarahernandez.17@gmail.com>
2026-04-30 00:19:38 -0300

    Add nutrition analysis and recipe generation functions

---
+++ agents.py
@@ -0,0 +1,9 @@
+from tools import analyze_image, generate_recipe
+
+
+def run_nutrition_analysis(image):
+    return analyze_image(image)
+
+
+def run_recipe_generation(image):
+    return generate_recipe(image)
