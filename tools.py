246f670837ed307ab888c82173c99b94293477e7
César Hernández <cesarahernandez.17@gmail.com>
2026-04-30 00:20:06 -0300

    Add image analysis and recipe generation functions

---
+++ tools.py
@@ -0,0 +1,9 @@
+import os
+
+
+def analyze_image(image):
+    return 'Calories: 520 | Protein: 32g | Balanced meal.'
+
+
+def generate_recipe(image):
+    return 'Suggested recipe based on detected ingredients.'
